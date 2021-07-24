b0b9cfdec2994c36c7132e3ebe54dbdd42a4073d
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-07-24 03:34:35 +0000

    Added CSS styling to idea components. #2

--- client/pages/ideas.jsx
+++ client/pages/ideas.jsx
@@ -4,17 +4,34 @@ function Idea(props) {
   const { title, description, address } = props.idea;
   // const idAttr = `idea-item-${ideaId}`;
   return (
-    <div>
-      {title}
-      {address}
-      {description}
+    <div className="idea-item">
+      <div className="idea-title color-dark-gray">
+        {title}
+      </div>
+      <div className="idea-address color-medium-gray">
+        <span className="fa fa-map-marker map-marker-icon color-medium-gray" aria-hidden="true"></span>
+        {address}
+      </div>
+      <div className="idea-description color-medium-gray">
+        {description}
+      </div>
+      <div className="row desktop-idea-buttons">
+        <span className="desktop-edit-button-container">
+          <span className="fas fa-edit desktop-idea-edit-icon color-dark-gray"></span>
+          <span className="desktop-idea-edit-label color-dark-gray">Edit</span>
+        </span>
+        <span className="desktop-calendar-button-container">
+          <span className="far fa-calendar-plus desktop-idea-calendar-icon color-dark-gray"></span>
+          <span className="desktop-idea-calendar-label color-dark-gray">Make It a Date</span>
+        </span>
+      </div>
     </div>
   );
 }
 
 function Ideas(props) {
   return (
-    <>
+    <div className="ideas-container">
     {
       props.ideas.map(idea => {
         return (
@@ -25,7 +42,7 @@ function Ideas(props) {
         );
       })
     }
-    </>
+    </div>
   );
 }
 
